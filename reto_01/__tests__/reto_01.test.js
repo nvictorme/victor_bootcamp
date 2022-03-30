4ca0529375f5d475a0ced8e08aafd6b94d4ccd3a
Victor Noguera <noguera.victor@gmail.com>
2022-03-30 15:57:06 -0400

    updated tests for reto_01

--- reto_01/__tests__/reto_01.test.js
+++ reto_01/__tests__/reto_01.test.js
@@ -5,7 +5,16 @@ describe("sumaObjetivo", () => {
     const arr = [3, 5, -4, 8, 11, 1, -1, 6];
     const objetivo = 10;
     const result = sumaObjetivo(arr, objetivo);
+    expect(result.length).toBe(2);
     expect(result.includes(-1)).toBeTruthy();
     expect(result.includes(11)).toBeTruthy();
+  });
+  it("retorna un array vacio", () => {
+    const arr = [3, 5, -4, 8, 1, 6];
+    const objetivo = 10;
+    const result = sumaObjetivo(arr, objetivo);
+    expect(result.length).toBe(0);
+    expect(result.includes(-1)).toBeFalsy();
+    expect(result.includes(11)).toBeFalsy();
   });
 });
