71cd44ab9bfa4f6a2bb8f0aa672106499201fe2a
Gianfranco Caschetto Campisi <papancho7@gmail.com>
2022-03-30 21:47:40 -0400

    Pa4sa el test que retorna 2 enteros y el4 que retorna el array vacío

--- reto_01/solucion.js
+++ reto_01/solucion.js
@@ -1,18 +1,18 @@
 // aca puedes importar modulos o definir variables/helpers
 
-const arr = [3, 5, -4, 8, 11, 1, -1, 6];
-const objetivo = 10;
+const arr = [3, 5, -4, 8, 11, 1, 6];
+const objetivo = 9;
 const result = sumaObjetivo(arr, objetivo);
 console.log(result);
 
 function sumaObjetivo(array, objetivo) {
   let res = [];
   for (let i=0; i<array.length; i++) {
-    for(let j=0; j<array.lengthcd ; j++){
-      if(array[i] != array[j]){
-        if(array[i] + array[j] == objetivo){
-          array[i].append(res);
-          array[j].append(res);
+    for(let j=0; j<array.length; j++){
+      if(i != j){
+        if((array[i] + array[j] == objetivo) && !(res.includes(array[i]))){
+          res.push(array[i]);
+          res.push(array[j]);
         }
       }
     }
