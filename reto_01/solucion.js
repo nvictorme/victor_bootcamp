61b00572381773bcfde8adf97d709870646f4e72
Jose Pineda <ja.pinedab1610@gmail.com>
2022-03-31 13:15:25 -0400

    Holis V1

--- reto_01/solucion.js
+++ reto_01/solucion.js
@@ -2,6 +2,16 @@
 
 function sumaObjetivo(array, objetivo) {
     // escribe tu solucion aqui
+    console.log('Por que ella no me ama?');
+    let result = [];
+    array.forEach(a => {
+      let b = objetivo - a;
+      let index = array.indexOf(b);
+      if (index != -1) {
+        result = [a, array[index]];
+      }
+    });
+    return result;
 }
   
   // no modificar despues de esta linea
