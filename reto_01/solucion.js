201150508c5881c769b68a8ed12ec595e400bfab
Gianfranco Caschetto Campisi <papancho7@gmail.com>
2022-03-30 22:56:37 -0400

    Corrección 1 finalizada

--- reto_01/solucion.js
+++ reto_01/solucion.js
@@ -1,24 +1,21 @@
 // aca puedes importar modulos o definir variables/helpers
-
-const arr = [3, 5, -4, 8, 11, 1, 6];
-const objetivo = 9;
-const result = sumaObjetivo(arr, objetivo);
-console.log(result);
-
 function sumaObjetivo(array, objetivo) {
   let res = [];
+  console.time("myTimer");
   for (let i=0; i<array.length; i++) {
     for(let j=0; j<array.length; j++){
       if(i != j){
-        if((array[i] + array[j] == objetivo) && !(res.includes(array[i]))){
+        if(array[i] + array[j] == objetivo){
           res.push(array[i]);
           res.push(array[j]);
+          return res;
         }
       }
     }
   }
+  console.timeEnd("myTimer");
   return res;
 }
-  
+
   // no modificar despues de esta linea
  module.exports = { sumaObjetivo }
