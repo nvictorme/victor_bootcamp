efee61f29e1c48988ca17e2855df59d359c11f90
Gianfranco Caschetto Campisi <papancho7@gmail.com>
2022-03-30 20:27:22 -0400

    Pasa la del arreglo vacío pero no pasa el de la suma

--- reto_01/reto_01.md
+++ reto_01/reto_01.md
@@ -5,7 +5,7 @@
 Escribe una función que tome un array no vacío de enteros distintos y un entero que represente una *suma objetivo*.
 
 #### Condiciones:
-Si dos números cualesquiera en el array de entrada suman lo mismo que la *suma objetivo*, la funcion debe devolverlos en un array, en cualquier orden.
+Si dos números cualquiera en el array de entrada suman lo mismo que la *suma objetivo*, la funcion debe devolverlos en un array, en cualquier orden.
 
 Si no hay dos números que sumen la *suma objetivo*, la función debería devolver un array vacío.
 
--- reto_01/solucion.js
+++ reto_01/solucion.js
@@ -1,7 +1,23 @@
 // aca puedes importar modulos o definir variables/helpers
 
+const arr = [3, 5, -4, 8, 11, 1, -1, 6];
+const objetivo = 10;
+const result = sumaObjetivo(arr, objetivo);
+console.log(result);
+
 function sumaObjetivo(array, objetivo) {
-    // escribe tu solucion aqui
+  let res = [];
+  for (let i=0; i<array.length; i++) {
+    for(let j=0; j<array.lengthcd ; j++){
+      if(array[i] != array[j]){
+        if(array[i] + array[j] == objetivo){
+          array[i].append(res);
+          array[j].append(res);
+        }
+      }
+    }
+  }
+  return res;
 }
   
   // no modificar despues de esta linea
