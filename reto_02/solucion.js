eb2b7d9d1b3573bc54ffbdd2cdabd409dbcfa953
Gianfranco Caschetto Campisi <papancho7@gmail.com>
2022-04-01 13:57:41 -0400

    Solucion al reto_02

--- reto_02/solucion.js
+++ reto_02/solucion.js
@@ -1,8 +1,19 @@
 // aca puedes importar modulos o definir variables/helpers
 
 function validarSubserie(arr, secuencia) {
-    // escribe tu solucion aqui
-}
-  
+  let pos = 0;
+  let res = [];
+  for (let i=0; i<arr.length; i++ ){
+    if(arr[i] == secuencia[pos]){
+      res.push(secuencia[pos]);
+      pos++;
+    }
+  }
+  if (res.length == secuencia.length){
+    return true;
+  }else{
+    return false;
+  }
+}  
   // no modificar despues de esta linea
  module.exports = { validarSubserie }
