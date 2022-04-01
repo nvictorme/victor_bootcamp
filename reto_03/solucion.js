8e0bb6e381615a541a3cb17e631a74966b53afe8
Gianfranco Caschetto Campisi <papancho7@gmail.com>
2022-04-01 15:23:43 -0400

    Solucion reto_03_Gian

--- reto_03/solucion.js
+++ reto_03/solucion.js
@@ -6,9 +6,15 @@
  * @return {number[]} array de cuadrados en orden creciente
  */
 function cuadradosOrdenados(arr) {
-    // escribe tu solucion aqui
-    return [];
+  let res = [];
+    for (let i=0; i<arr.length; i++){
+      res.push(arr[i]**2);
+    }
+    res.sort(function(a, b) {
+      return a - b;
+    });
+    return res;
 }
-  
+
   // no modificar despues de esta linea
  module.exports = { cuadradosOrdenados }
